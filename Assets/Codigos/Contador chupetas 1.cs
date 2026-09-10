53fd91b9cc8fd3af981dd9078ad45763ab854050
carlos09yyl <167011932+carlos09yyl@users.noreply.github.com>
2026-09-10 16:09:27 -0500

    aCTUALIZACION
    trabajo en classe

---
+++ Assets/Codigos/Contador chupetas 1.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+public class Contadorchupetas1 : MonoBehaviour
+{
+    public int contador = 0;
+    public TextMeshProUGUI Texto_Contador_TMPro;
+    public AudioSource sonidoAumentar;
+    public AudioSource sonidoDisminuir;
+
+    public void AumentarContador()
+    {
+
+    }
+
+    public void DisminuirContador()
+    {
+
+    }
+
+
+    public void IniciarContador()
+    {
+
+    }
+
+
+    public void AlIniciarScript()
+    {
+
+    }
+
+
+
+    public void AlDesactivarScript()
+    {
+
+    }
+}
---
+++ Assets/Codigos/contador de chupetas 2.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class contadordechupetas2 : MonoBehaviour
+{
+
+}
